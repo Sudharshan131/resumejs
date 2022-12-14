cdf29874fe665d4ea60655be4a62be43f4219abd
Sudharshan131 <114296916+Sudharshan131@users.noreply.github.com>
2022-12-14 10:26:57 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,65 @@
+function resume(){
+    let x;
+    x=document.getElementById('fname').value;
+    console.log(x);
+    let y;
+    y=document.getElementById('mname').value;
+    console.log(y);
+    let z;
+    z=document.getElementById('lname').value;
+    console.log(z);
+    document.getElementById('name').innerHTML=x+y+z;
+
+    let a;
+    a=document.getElementById("faname").value;
+    console.log(a);
+    document.getElementById("finame").innerHTML=a;
+
+    let b;
+    b=document.getElementById('dofb').value;
+    console.log(b);
+    document.getElementById('dob').innerHTML=b;
+
+    let c;
+    c=document.getElementById('phonum').value;
+    console.log(c);
+    document.getElementById('phno').innerHTML=c;
+
+    let d;
+    d=document.getElementById('mai').value;
+    console.log(d);
+    document.getElementById('mail').innerHTML=d;
+
+    let e;
+    e=document.getElementById('s1').value;
+    console.log(e);
+    document.getElementById('addres').innerHTML=e;
+
+
+    let f;
+    f=document.getElementById('s2').value;
+    console.log(f);
+    document.getElementById('addres').innerHTML=f;
+    
+    let g;
+    g=document.getElementById('s3').value;
+    console.log(g);
+    document.getElementById('addres').innerHTML=g;
+
+    let h;
+    h=document.getElementById('s4').value;
+    console.log(h);
+    document.getElementById('addres').innerHTML=h;
+
+    let i;
+    i=document.getElementById('s5').value;
+    console.log(i);
+    document.getElementById('addres').innerHTML=i;
+
+    let j;
+    j=document.getElementById('s6').value;
+    comsole.log(j);
+    document.getElementById('addres').innerHTML=j;
+
+   
+}
